83710b5f8029cdee657aed56b802308beafcd1a1
guttomarttins <guttomarttins@gmail.com>
2023-11-11 16:01:36 -0300

    microservice auth

---
+++ stateless/stateless-auth-api/src/main/java/br/com/microservice/core/model/User.java
@@ -0,0 +1,32 @@
+package br.com.microservice.core.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user", schema = "public")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+    
+}
